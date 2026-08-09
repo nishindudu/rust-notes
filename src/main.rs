49347a797323f0f6a6fcf64a393d46f645199311
Nishin Santhosh <nishindudu@gmail.com>
2026-08-09 22:09:59 +0530

    Hide console for release builds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] //Hides console on release builds
+
 use std::path::{PathBuf};
 use std::fmt::Error;
 use iced::{Alignment, Length, Task};
